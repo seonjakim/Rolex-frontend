22ed3e132d5e51439807ae48f045420ed54c4236
oneiron <admin@adminui-MacBookPro.local>
2020-04-27 15:16:46 +0900

    modify Conflicting files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-<<<<<<< HEAD
-import './Styles/Reset.scss'
-=======
+
 import './Styles/Reset.scss';
->>>>>>> master
 import './Styles/Common.scss'
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
