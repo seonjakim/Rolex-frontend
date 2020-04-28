5c849d1c2b4bff346574c189654fa25296c6c4a1
oneiron <admin@adminui-MacBookPro.local>
2020-04-28 19:12:48 +0900

    first commit

--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import './TopNav.scss';
+import React, { Component } from "react";
+import "./TopNav.scss";
 
 class TopNav extends Component {
-    render(){
-        return(
-            <div className='TopNav'>
-                here is TopNav
-            </div>
-        )
-    }
+  render() {
+    return <div className="TopNav">here is TopNav</div>;
+  }
 }
 
 export default TopNav;
