a7383124c69d377c317d8f9b38b7b47ca71a91be
Minseok Lee <maldarkmlee@gmail.com>
2020-04-28 20:08:19 +0900

    first backup

--- src/Pages/SettingRolex/SettingRolex.js
+++ src/Pages/SettingRolex/SettingRolex.js
@@ -4,7 +4,36 @@ import "./SettingRolex.scss";
 
 class SettingRolex extends Component {
   render() {
-    return <div className="SettingRolex">here is SettingRolex</div>;
+    return (
+      <div className="SettingRolex">
+        <div className="settingZone">
+          <div className="configurator">
+            <p></p>
+            <p></p>
+            <p></p>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className="btnSection">
+            <div className="btnContainer">
+              <button></button>
+              <button></button>
+              <button></button>
+              <div className="emptyBuffer" />
+            </div>
+          </div>
+        </div>
+
+        <div>Footer</div>
+      </div>
+    );
   }
 }
 
