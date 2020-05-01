b780ca494523d8b08c0909e4fd44c7b999cc090d
oneiron <admin@adminui-MacBookPro.local>
2020-05-01 16:35:11 +0900

    last

--- src/Pages/SignInNUp/SignIn.js
+++ src/Pages/SignInNUp/SignIn.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
-import ColorLogo from './ColorLogo.png'
-import { API_URL } from '../../Config';
-import './SignIn.scss';
-
+import React from "react";
+import { withRouter, Link } from "react-router-dom";
+import ColorLogo from "./ColorLogo.png";
+import "./SignIn.scss";
 
 class SignIn extends React.Component {
   constructor() {
@@ -44,23 +42,33 @@ class SignIn extends React.Component {
           <h2>로그인</h2>
           <div className="IdPwInput">
             <div>이름</div>
-            <input className="EmailAddress" onChange={(e) => { this.setState({ name: e.target.value }) }}></input>
+            <input
+              className="EmailAddress"
+              onChange={(e) => {
+                this.setState({ name: e.target.value });
+              }}
+            ></input>
           </div>
           <div className="IdPwInput">
             <div>비밀번호</div>
-            <input className="PW" onChange={(e) => { this.setState({ password: e.target.value }) }} type="password"></input>
+            <input
+              className="PW"
+              onChange={(e) => {
+                this.setState({ password: e.target.value });
+              }}
+              type="password"
+            ></input>
           </div>
           <div className="WraptheBtn">
-            <button onClick={this.signIn}>
-              로그인
-                            </button>
-            <button className="Join" ><Link to="/signup">회원가입</Link></button>
+            <button onClick={this.signIn}>로그인</button>
+            <button className="Join">
+              <Link to="/signup">회원가입</Link>
+            </button>
           </div>
           <div className="FindIdPw">아이디 / 비밀번호 찾기</div>
         </div>
       </div>
-    )
+    );
   }
-
 }
 export default withRouter(SignIn);
--- src/Pages/SignInNUp/SignUp.js
+++ src/Pages/SignInNUp/SignUp.js
@@ -1,81 +1,94 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import ColorLogo from './ColorLogo.png'
-import { API_URL } from '../../Config';
-import './SignUp.scss';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import ColorLogo from "./ColorLogo.png";
+import { API_URL } from "../../Config";
+import "./SignUp.scss";
 
 class SignUp extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            name: "",
-            password: "",
-            checkpassword: "",
-            signUpData: []
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      password: "",
+      checkpassword: "",
+      signUpData: [],
+    };
+  }
 
-    clickToSignUp = (e) => {
-        e.preventDefault();
-        const { name, password, checkPassword } = this.state;
-        if (!name || !password || !checkPassword) {
-            alert("모든 항목을 작성해주세요.")
-        } else if (password !== checkPassword) {
-            alert("비밀번호를 다시 한번 확인해주세요.")
+  clickToSignUp = (e) => {
+    e.preventDefault();
+    const { name, password, checkPassword } = this.state;
+    if (!name || !password || !checkPassword) {
+      alert("모든 항목을 작성해주세요.");
+    } else if (password !== checkPassword) {
+      alert("비밀번호를 다시 한번 확인해주세요.");
+    } else {
+      fetch(`http://10.58.5.93:8000/user/sign-up`, {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          password,
+        }),
+      }).then((res) => {
+        if (res.status === 401) {
+          alert("다시 한 번 확인해주세요.");
         } else {
-            fetch(`${API_URL}/user/sign-up`, {
-                method: "POST",
-                body: JSON.stringify({
-                    name,
-                    password,
-                }),
-            })
-                .then(res => {
-                    if (res.status === 401) {
-                        alert("다시 한 번 확인해주세요.")
-                    } else {
-                        alert("가입 완료");
-                        this.props.history.push('/signin')
-                    }
-                })
+          alert("가입 완료");
+          this.props.history.push("/signin");
         }
+      });
     }
+  };
 
-
-    render() {
-        return (
-            <div className="SignUp">
-                <div className="ContentWrapper">
-                    <div className="Logo">
-                        <img src={ColorLogo} alt="color logo" />
-                    </div>
-                    <h2>회원가입</h2>
-                    <div className="IdPwInput">
-                        <div>이름</div>
-                        <input className="UserName" onChange={(e) => { this.setState({ name: e.target.value }) }}></input>
-                    </div>
-                    <div className="IdPwInput">
-                        <div>비밀번호</div>
-                        <input type="password" className="PWInSignUp" onChange={(e) => { this.setState({ password: e.target.value }) }}></input>
-                    </div>
-                    <div className="IdPwInput">
-                        <div>비밀번호 확인</div>
-                        <input type="password" className="CheckPW" onChange={(e) => { this.setState({ checkPassword: e.target.value }) }}></input>
-                    </div>
-                    <div className="AgreeJoin">
-                        <button type="checkbox"></button>
-                        <div>이용약관 동의</div>
-                    </div>
-                    <div className="WraptheBtn">
-                        <button onClick={this.clickToSignUp}>
-                            회원가입
-                        </button>
-                        <button className="Join">로그인</button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="SignUp">
+        <div className="ContentWrapper">
+          <div className="Logo">
+            <img src={ColorLogo} alt="color logo" />
+          </div>
+          <h2>회원가입</h2>
+          <div className="IdPwInput">
+            <div>이름</div>
+            <input
+              className="UserName"
+              onChange={(e) => {
+                this.setState({ name: e.target.value });
+              }}
+            ></input>
+          </div>
+          <div className="IdPwInput">
+            <div>비밀번호</div>
+            <input
+              type="password"
+              className="PWInSignUp"
+              onChange={(e) => {
+                this.setState({ password: e.target.value });
+              }}
+            ></input>
+          </div>
+          <div className="IdPwInput">
+            <div>비밀번호 확인</div>
+            <input
+              type="password"
+              className="CheckPW"
+              onChange={(e) => {
+                this.setState({ checkPassword: e.target.value });
+              }}
+            ></input>
+          </div>
+          <div className="AgreeJoin">
+            <button type="checkbox"></button>
+            <div>이용약관 동의</div>
+          </div>
+          <div className="WraptheBtn">
+            <button onClick={this.clickToSignUp}>회원가입</button>
+            <button className="Join">로그인</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(SignUp);
